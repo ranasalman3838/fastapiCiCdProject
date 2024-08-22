d13bdfa63984cacd0c713331542783f0a80a3d33
Xeven <salman38>
2024-08-22 17:57:03 +0500

    message changes

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello, World!"}
+    return {"message": "Hello, World cicd done!"}
 
 if __name__ == "__main__":
     import uvicorn
